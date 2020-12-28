82385b78554dcd0f54f2a4942dd7c30079d1cc44
suman mandal <48859011+suman1buie@users.noreply.github.com>
2020-12-28 10:13:36 +0530

    Update getAll.js

--- js/getAll.js
+++ js/getAll.js
@@ -3,6 +3,7 @@ let Name = localStorage.getItem("name");
 let phone = localStorage.getItem("phoneNumber");
 let email = localStorage.getItem("email");
 let hoby = localStorage.getItem("hoby");
+let message = localStorage.getItem("message");
 
 //create all html tag regarding there details
 let mainDiv = document.getElementById("main");
@@ -10,12 +11,14 @@ let nameItem = document.createElement("h2");
 let phoneItem = document.createElement("h4");
 let emailItem = document.createElement("h4");
 let hobyItem = document.createElement("h6");
+let messageItem = document.createElement("p");
 
 //put data into that html
 nameItem.innerHTML = "Name is :" + "  " + Name;
 phoneItem.innerHTML = "Phone number :" + "  " + phone;
 emailItem.innerHTML = "Email is :  " + email;
 hobyItem.innerHTML = "Hoby is: " + hoby;
+messageItem.innerHTML = "<p>Question is : </p>" + message;
 
 //style the div
 mainDiv.style.textAlign = "center";
@@ -28,3 +31,4 @@ mainDiv.appendChild(nameItem);
 mainDiv.appendChild(phoneItem);
 mainDiv.appendChild(emailItem);
 mainDiv.appendChild(hobyItem);
+mainDiv.appendChild(messageItem);
